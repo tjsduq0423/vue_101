2afbd94465ea9308b1ddd31bd95da6f8377f99ee
Joon ho <tjsduq0423@uos.ac.kr>
2021-09-26 23:12:09 +0900

    컴포넌트 생성님 연결 데이터 바인딩 + props 문법

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Modal :누른거="누른거" :모달창열렸니="모달창열렸니" :원룸들="원룸들" />
+
+  <div class="menu">
+    <a v-for="메뉴 in 메뉴들" :key="메뉴">{{ 메뉴 }}</a>
+  </div>
+
+  <Discount />
+
+  <Card :원룸="원룸" v-for="원룸 in 원룸들" :key="원룸" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from "./assets/oneroom.js";
+import Discount from "./components/Discount.vue";
+import Modal from "./components/Modal.vue";
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      누른거: 0,
+      원룸들: data,
+      모달창열렸니: false,
+      메뉴들: ["Home", "Products", "About"],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Discount: Discount,
+    Modal: Modal,
+    Card: Card,
+  },
+};
 </script>
 
 <style>
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="black-bg" v-if="모달창열렸니">
+    <div class="white-bg">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <p>{{ 원룸들[누른거].content }}</p>
+      <!-- <button @click="모달창열렸니 = false">닫기</button> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    원룸들: Array,
+    누른거: Number,
+    모달창열렸니: Boolean,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
